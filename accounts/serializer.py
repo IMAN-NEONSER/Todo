a768c3dd1edbe59984e2de2ee39f88af13b9de67
iman <imanmozafari.neonser@gmail.com>
2024-08-14 19:37:50 +0330

    add method to check username not contain the admin or root

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -10,3 +10,8 @@ class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     email = serializers.EmailField(required=True, validators=[clean_email])
     password = serializers.CharField(required=True, write_only=True)
+
+    def validate_username(self, value):
+        if value == 'admin' or 'root':
+            raise serializers.ValidationError('username cant be admin or root')
+        return value
