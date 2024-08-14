46ef7a29af3d37b014ba3842d0b8d7580af92b3b
iman <imanmozafari.neonser@gmail.com>
2024-08-14 19:46:04 +0330

    add method to check password and comfirmpassword is same.

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -10,8 +10,14 @@ class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     email = serializers.EmailField(required=True, validators=[clean_email])
     password = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
 
     def validate_username(self, value):
         if value == 'admin' or 'root':
             raise serializers.ValidationError('username cant be admin or root')
         return value
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('passwords must match')
+        return data
