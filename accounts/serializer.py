3e3ead5c21b2c6e086c4dd442e7842c649279f2d
iman <imanmozafari.neonser@gmail.com>
2024-08-14 23:09:59 +0330

    override create

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 def clean_email(value):
@@ -6,11 +7,20 @@ def clean_email(value):
         raise serializers.ValidationError('admin or root cant be in email')
 
 
-class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True, validators=[clean_email])
-    password = serializers.CharField(required=True, write_only=True)
-    password2 = serializers.CharField(required=True, write_only=True)
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'validators': (clean_email,)},
+        }
+
+    def create(self, validated_data):
+        del validated_data['password2']
+        return User.objects.create_user(**validated_data)
 
     def validate_username(self, value):
         if value == 'admin' or 'root':
