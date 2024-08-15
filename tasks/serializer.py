c2366add4daecaf08245325df235433260b8019d
iman <imanmozafari.neonser@gmail.com>
2024-08-15 16:14:59 +0330

    create addtaskssrializer

--- tasks/serializer.py
+++ tasks/serializer.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from .models import Tasks
+
+
+class AddTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tasks
+        fields = ('body', )
