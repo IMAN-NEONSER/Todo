0aff139507f48160725f270af866e017a58281da
iman <imanmozafari.neonser@gmail.com>
2024-08-17 21:52:52 +0330

    create logout view and url for this view.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view()),
     path('create_auth_token/', auth_views.obtain_auth_token),
+    path('logout/', views.UserLogoutView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from .serializer import UserRegisterSerializer
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
 
 
 # Create your views here.
@@ -13,3 +15,15 @@ class UserRegisterView(APIView):
             ser_data.create(ser_data.validated_data)
             return Response(ser_data.data, status=status.HTTP_201_CREATED)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            token = Token.objects.get(user=request.user)
+            token.delete()
+            return Response(status=status.HTTP_200_OK)
+        except Token.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
