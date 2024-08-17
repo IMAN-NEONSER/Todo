aae2f5bdf3178c0952bf1722d8fc7d3e6922c91a
iman <imanmozafari.neonser@gmail.com>
2024-08-17 20:22:40 +0330

    create url for create auth token.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken import views as auth_views
 
 
 app_name = 'accounts'
 urlpatterns = [
-    path('register/', views.UserRegisterView.as_view())
+    path('register/', views.UserRegisterView.as_view()),
+    path('create_auth_token/', auth_views.obtain_auth_token),
 ]
